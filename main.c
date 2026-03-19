bcf331bea4f4ac949fb3b74ef5775e2f01368349
Olen T <elroylilly@gmail.com>
2026-03-19 19:46:54 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+Linux:
+
+=============================
+
+# Bootloader:
+
+#include <stdio.h>
+#include <unistd.h>
+
+#DEFINE BAD_ERROR 0xFF67
+
+int main() 
+{
+	printf("Initializing Grub...");
+  usleep(670000);
+  printf("Done");
+  
+  printf("Detecting Drives...")
+  sleep(1); // ??????? IDK
+  
+  printf("Loading Kernel Image...")
+  if !kernel.image {
+  	print('This is the image')
+  }
+  printf("Loaded the matrix PE ELF File"); //finnaly getting somewhere
+  
+  
+  return BAD_ERROR;
+}
+
+===========================NOTES=========================================
+WHO WANNA BE PROGRESS SAVER INCASE IT GET DELETED? COPY IT EVERY SO OFTEN
+Guys semicolons are after C lines...
